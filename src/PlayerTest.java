e94f47dae395d9cea4a7fc1d5edfaf77b1a0a40a
rjbowenUdel <rjbowen@udel.edu>
2017-09-23 17:41:18 -0400

    Full coverage of the player class tested

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -35,5 +35,44 @@ public class PlayerTest {
 		assertEquals(0, player.getScore());
 	}
 	
+	@Test
+	public void testSetXCoord() {
+		player.setXcoord(21);
+		assertEquals(21, player.getX());
+	}
+	
+	@Test
+	public void testSetYCoord() {
+		player.setYcoord(19);
+		assertEquals(19, player.getY());
+	}
+	
+	@Test
+	public void testMoveLeft() {
+		player.moveLeft();
+		
+		assertEquals(1, player.getScore());
+		assertEquals(testWidth / 2 - 1, 3);
+	}
+	
+	@Test
+	public void testMoveRight() {
+		player.moveRight();
+		
+		assertEquals(1, player.getScore());
+		assertEquals(testWidth / 2 + 1, 5);
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("X", player.toString());
+	}
+	
+	@Test
+	public void testUpScore() {
+		player.upScore();
+		assertEquals(1, player.getScore());
+	}
+	
 	
 }
