8e46e8b3721516ddf724950e61c4e43a94a498c8
rjbowenUdel <rjbowen@udel.edu>
2017-09-23 17:32:48 -0400

    Added more test methods to the player test class. About half way done now

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -6,10 +6,34 @@ import org.junit.Test;
 
 public class PlayerTest {
 
-	Player player = new Player(3, 8);
-
+	int testWidth = 8;
+	int testHeight = 3;
+	
+	Player player = new Player(testHeight, testWidth);
+	private long playerCreateTime = System.currentTimeMillis();
+	
+	
 	@Test
 	public void testGetX() {
-		
+		int obtainedX = player.getX();
+		assertEquals(testWidth / 2, obtainedX);
 	}
+	
+	@Test
+	public void testGetY() {
+		int obtainedY = player.getY();
+		assertEquals(testHeight, obtainedY);
+	}
+	
+	@Test
+	public void testGetTime() {
+		assertEquals(System.currentTimeMillis() - playerCreateTime, player.getTime());
+	}
+	
+	@Test
+	public void testGetScore() {
+		assertEquals(0, player.getScore());
+	}
+	
+	
 }
