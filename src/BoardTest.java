43176f5ed8c4505ac857108f6cd06fbc53999b50
rjbowenUdel <rjbowen@udel.edu>
2017-09-25 23:10:43 -0400

    Made some changes to the board test

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -8,11 +11,15 @@ import org.junit.Test;
 
 public class BoardTest {
 
-	protected int dif = 1;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	protected int dif = 49;
 	protected boolean lst = false; // lost
 	protected int timr = 150;
 	private int x_y = 20;
 	protected Player orig_player = new Player(x_y, x_y);
+	Board test_board = new Board(dif);
+	
 
 	@Test
 	public void test_easy() {
@@ -34,9 +41,8 @@ public class BoardTest {
 		assertEquals(test_board.player.getX(), orig_player.getX());
 
 		// testing to walls
-		for (int i = 0; i < 60; i++) {
-			test_board.processChar('o');
-		}
+		test_board.processChar('o');
+		
 		for (int i = 0; i < 60; i++) {
 			test_board.processChar('p');
 		}
@@ -44,6 +50,76 @@ public class BoardTest {
 		// test upScore()
 		test_board.processChar('k');
 
+	}
+	
+	@Test
+	public void test_scoringTierZero() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(0, 1);
+		
+		assertEquals("Wow...uhm...so...wow...", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierOne() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(151, 1);
+		
+		assertEquals("You tried...", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierTwo() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(201, 1);
+		
+		assertEquals("Are you even using the keys?", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierThree() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(301, 1);
+		
+		assertEquals("Pff *rolls eyes*", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierFour() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(401, 1);
+		
+		assertEquals("You are getting better at this :D", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierFive() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(501, 1);
+		
+		assertEquals("Holy Canoli, you are a monster!", outContent.toString().trim());
+	}
+	
+	@Test
+	public void test_scoringTierSix() {
+		
+		System.setOut(new PrintStream(outContent));
+		
+		test_board.scoring(651, 1);
+		
+		assertEquals("You are a true champion!", outContent.toString().trim());
 	}
 
 }
